94fdf6220b59cdc1370c685e817e19bcdb8c4fa9
Almas Kureshi <almaskureshi099@gmail.com>
2026-05-26 09:43:32 +0530

    hardcode api url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,17 +1,10 @@
 import axios from "axios";
 
-const API_URL =
-  import.meta.env.VITE_API_URL ||
-  "https://talent-iq-backend-kflz.onrender.com/api";
-
 const axiosInstance = axios.create({
-  baseURL: API_URL,
-  withCredentials: false,
+  baseURL: "https://talent-iq-backend-kflz.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-console.log("API URL:", API_URL);
 
 export default axiosInstance;
