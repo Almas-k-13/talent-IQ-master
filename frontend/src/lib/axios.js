e272e7cfa307f9e431ebda544c2a7ae04a4cb6ea
Almas Kureshi <almaskureshi099@gmail.com>
2026-05-26 09:48:57 +0530

    fix final api routes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,9 +2,12 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://talent-iq-backend-kflz.onrender.com/api",
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+console.log("API URL:", axiosInstance.defaults.baseURL);
 
 export default axiosInstance;
