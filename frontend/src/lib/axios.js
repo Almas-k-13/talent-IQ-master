3bf818112858f9dd3cea25091917c05ebbd93b2b
Almas Kureshi <almaskureshi099@gmail.com>
2026-05-27 08:50:13 +0530

    debug api

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,11 +2,42 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://talent-iq-backend-kflz.onrender.com/api",
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-console.log("API URL:", axiosInstance.defaults.baseURL);
+// REQUEST DEBUG
+axiosInstance.interceptors.request.use(
+  (config) => {
+    console.log("🚀 API REQUEST =>", {
+      method: config.method,
+      fullURL: `${config.baseURL}${config.url}`,
+    });
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// RESPONSE DEBUG
+axiosInstance.interceptors.response.use(
+  (response) => {
+    console.log("✅ API RESPONSE =>", response.config.url, response.data);
+    return response;
+  },
+  (error) => {
+    console.log("❌ API ERROR =>", {
+      url: error?.config?.url,
+      fullURL: `${error?.config?.baseURL}${error?.config?.url}`,
+      response: error?.response?.data,
+    });
+
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
