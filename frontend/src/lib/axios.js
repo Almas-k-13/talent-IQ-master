95ab503d12c35aa39fc246439f71d87c8c5315c2
Almas Kureshi <almaskureshi099@gmail.com>
2026-05-27 09:43:33 +0530

    bug fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,32 +2,43 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://talent-iq-backend-kflz.onrender.com/api",
-  withCredentials: false,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// DEBUG
-axiosInstance.interceptors.request.use((config) => {
-  console.log("🚀 FINAL API URL =>", {
-    method: config.method,
-    url: config.url,
-    fullURL: `${config.baseURL}${config.url}`,
-  });
+// REQUEST LOGGER
+axiosInstance.interceptors.request.use(
+  (config) => {
+    console.log("🚀 REQUEST =>", {
+      method: config.method,
+      url: config.url,
+      baseURL: config.baseURL,
+      fullURL: `${config.baseURL}${config.url}`,
+    });
 
-  return config;
-});
+    return config;
+  },
+  (error) => {
+    console.log("❌ REQUEST ERROR", error);
+    return Promise.reject(error);
+  }
+);
 
+// RESPONSE LOGGER
 axiosInstance.interceptors.response.use(
   (response) => {
-    console.log("✅ API RESPONSE =>", response.config.url, response.data);
+    console.log("✅ RESPONSE =>", {
+      url: response.config.url,
+      data: response.data,
+    });
+
     return response;
   },
   (error) => {
     console.log("❌ API ERROR =>", {
       url: error.config?.url,
-      fullURL: `${error.config?.baseURL}${error.config?.url}`,
       response: error.response?.data,
     });
 
